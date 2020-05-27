af24e0d95e8bfa52e99ef0d37de1f75eec2e087d
Palina Paulichenka <64165131+palina-pauliuchenka@users.noreply.github.com>
2020-05-27 11:07:05 +0300

    Delete main.js

--- project/public/assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-const appView = new View(),
-  appModel = new Model(),
-  appController = new Controller(),
-  container = document.getElementById('app');
-
-appView.init(container);
-appModel.init(appView);
-appController.init(appModel, container);
